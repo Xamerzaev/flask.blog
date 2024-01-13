d76df862d6f968b83ae11560817e108d836356ea
Mahamerz <88981557+Xamerzaev@users.noreply.github.com>
2024-01-13 18:38:49 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = "eaf32487r32h7r8hf3287rh372iq"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///blog.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
